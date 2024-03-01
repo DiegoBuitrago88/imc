189c1244f38f508f221c158e78a9225b1676e9fb
DiegoBuitrago88 <158835238+DiegoBuitrago88@users.noreply.github.com>
2024-03-01 16:34:24 -0500

    programa de imc

---
+++ src/main/java/com/mycompany/imc/IMC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.imc;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class IMC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
